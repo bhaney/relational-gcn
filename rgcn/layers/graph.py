bdb2c5175469b0a14edb365bbc81a9b763a0b774
Bijan Haney <bijan.haney@consensys.net>
2019-01-07 15:34:02 -0500

    updated for keras version

--- rgcn/layers/graph.py
+++ rgcn/layers/graph.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-from keras import activations, initializations
+from keras import activations, initializers
 from keras import regularizers
 from keras.engine import Layer
 from keras.layers import Dropout
@@ -10,9 +10,9 @@ import keras.backend as K
 
 class GraphConvolution(Layer):
     def __init__(self, output_dim, support=1, featureless=False,
-                 init='glorot_uniform', activation='linear',
-                 weights=None, W_regularizer=None, num_bases=-1,
-                 b_regularizer=None, bias=False, dropout=0., **kwargs):
+                 kernel_initializer='glorot_uniform', activation='linear',
+                 weights=None, kernel_regularizer=None, num_bases=-1,
+                 bias_regularizer=None, use_bias=False, dropout=0., **kwargs):
         self.init = initializations.get(init)
         self.activation = activations.get(activation)
         self.output_dim = output_dim  # number of features per node
@@ -22,18 +22,18 @@ class GraphConvolution(Layer):
 
         assert support >= 1
 
-        self.W_regularizer = regularizers.get(W_regularizer)
-        self.b_regularizer = regularizers.get(b_regularizer)
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
 
-        self.bias = bias
+        self.use_bias = use_bias
         self.initial_weights = weights
         self.num_bases = num_bases
 
         # these will be defined during build()
         self.input_dim = None
-        self.W = None
-        self.W_comp = None
-        self.b = None
+        self.kernel = None
+        self.kernel_comp = None
+        self.bias = None
         self.num_nodes = None
 
         super(GraphConvolution, self).__init__(**kwargs)
@@ -51,28 +51,28 @@ class GraphConvolution(Layer):
         self.input_dim = features_shape[1]
 
         if self.num_bases > 0:
-            self.W = K.concatenate([self.add_weight((self.input_dim, self.output_dim),
+            self.kernel = K.concatenate([self.add_weight((self.input_dim, self.output_dim),
                                                     initializer=self.init,
-                                                    name='{}_W'.format(self.name),
-                                                    regularizer=self.W_regularizer) for _ in range(self.num_bases)],
+                                                    name='{}_kernel'.format(self.name),
+                                                    regularizer=self.kernel_regularizer) for _ in range(self.num_bases)],
                                    axis=0)
 
-            self.W_comp = self.add_weight((self.support, self.num_bases),
+            self.kernel_comp = self.add_weight((self.support, self.num_bases),
                                           initializer=self.init,
-                                          name='{}_W_comp'.format(self.name),
-                                          regularizer=self.W_regularizer)
+                                          name='{}_kernel_comp'.format(self.name),
+                                          regularizer=self.kernel_regularizer)
         else:
-            self.W = K.concatenate([self.add_weight((self.input_dim, self.output_dim),
+            self.kernel = K.concatenate([self.add_weight((self.input_dim, self.output_dim),
                                                     initializer=self.init,
-                                                    name='{}_W'.format(self.name),
-                                                    regularizer=self.W_regularizer) for _ in range(self.support)],
+                                                    name='{}_kernel'.format(self.name),
+                                                    regularizer=self.kernel_regularizer) for _ in range(self.support)],
                                    axis=0)
 
-        if self.bias:
-            self.b = self.add_weight((self.output_dim,),
+        if self.use_bias:
+            self.bias = self.add_weight((self.output_dim,),
                                      initializer='zero',
-                                     name='{}_b'.format(self.name),
-                                     regularizer=self.b_regularizer)
+                                     name='{}_bias'.format(self.name),
+                                     regularizer=self.bias_regularizer)
 
         if self.initial_weights is not None:
             self.set_weights(self.initial_weights)
@@ -92,34 +92,34 @@ class GraphConvolution(Layer):
         supports = K.concatenate(supports, axis=1)
 
         if self.num_bases > 0:
-            self.W = K.reshape(self.W,
+            self.kernel = K.reshape(self.kernel,
                                (self.num_bases, self.input_dim, self.output_dim))
-            self.W = K.permute_dimensions(self.W, (1, 0, 2))
-            V = K.dot(self.W_comp, self.W)
+            self.kernel = K.permute_dimensions(self.kernel, (1, 0, 2))
+            V = K.dot(self.kernel_comp, self.kernel)
             V = K.reshape(V, (self.support*self.input_dim, self.output_dim))
             output = K.dot(supports, V)
         else:
-            output = K.dot(supports, self.W)
+            output = K.dot(supports, self.kernel)
 
         # if featureless add dropout to output, by elementwise multiplying with column vector of ones,
         # with dropout applied to the vector of ones.
         if self.featureless:
             tmp = K.ones(self.num_nodes)
             tmp_do = Dropout(self.dropout)(tmp)
-            output = (output.T * tmp_do).T
+            output = K.transpose(K.transpose(output) * tmp_do)
 
-        if self.bias:
-            output += self.b
+        if self.use_bias:
+            output = K.bias_add(output, self.bias, data_format='channels_last')
         return self.activation(output)
 
     def get_config(self):
         config = {'output_dim': self.output_dim,
                   'init': self.init.__name__,
                   'activation': self.activation.__name__,
-                  'W_regularizer': self.W_regularizer.get_config() if self.W_regularizer else None,
-                  'b_regularizer': self.b_regularizer.get_config() if self.b_regularizer else None,
+                  'kernel_regularizer': self.kernel_regularizer.get_config() if self.kernel_regularizer else None,
+                  'bias_regularizer': self.bias_regularizer.get_config() if self.bias_regularizer else None,
                   'num_bases': self.num_bases,
-                  'bias': self.bias,
+                  'use_bias': self.use_bias,
                   'input_dim': self.input_dim}
         base_config = super(GraphConvolution, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
